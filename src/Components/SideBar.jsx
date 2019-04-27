6e5f150ae8c6207da52083c27741bb6efa83b223
AntonDeXy <podolyananton@gmail.com>
2019-04-27 22:54:21 +0300

    initial commit

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SideBar = () => (
+  <nav>
+    <div>
+      <a href='#'>Profile</a>
+    </div>
+    <div>
+      <a href='#'>Messages</a>
+    </div>
+    <div>
+      <a href='#'>Music</a>
+    </div>
+    <div>
+      <a href='#'>Setting</a>
+    </div>
+  </nav>
+)
+
+export default SideBar
