834641e3070f18ef7b55a71930f5401115772ee7
DeXy <podolyananton@gmail.com>
2019-09-08 19:20:08 +0300

    87

--- src/Components/Post/MyPosts.jsx
+++ src/Components/Post/MyPosts.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {PureComponent} from 'react'
 import Post from './Post'
 import { Field, reduxForm } from 'redux-form';
 import { required, maxLenghtCreator } from '../../utils/validators/validators';
@@ -9,37 +9,26 @@ const maxLenght10 = maxLenghtCreator(10)
 const AddNewPostForm = (props) => {
   return (
     <form onSubmit={props.handleSubmit}>
-      {/* <textarea
-          onChange={OnPostChange}
-          ref={newPostElement}
-          value={props.newPostText}
-        /> */}
       <Field name='newPostText' component={Textarea} placeholder={"Post message"} validate={[required, maxLenght10]} />
       <button>Add post</button>
     </form>
   )
 }
 
-const MyPosts = props => {
-  let newPostElement = React.createRef()
-
-  let onAddPost = (values) => {
-    props.addPost(values.newPostText)
-  }
-
-  // let OnPostChange = () => {
-  //   let text = newPostElement.current.value
-  //   props.updateNewPostText(text)
-  // }
-
-  return (
-    <div className='MyPosts'>
-      <h3>My posts</h3>
-      <AddNewPostFormRedux onSubmit={onAddPost} />
-      {props.posts.map(post => <Post message={post.message} likeCount={post.likes} />)}
-    </div>
-  )
-}
+// but it works good withouth React.memo
+const MyPosts = React.memo(props => {
+    let onAddPost = (values) => {
+      props.addPost(values.newPostText)
+    }
+  
+    return (
+      <div className='MyPosts'>
+        <h3>My posts</h3>
+        <AddNewPostFormRedux onSubmit={onAddPost} />
+        {props.posts.map(post => <Post message={post.message} likeCount={post.likes} />)}
+      </div>
+    )
+})
 
 const AddNewPostFormRedux = reduxForm({
   form: "Profile"
